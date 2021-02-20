aa93f3d90c4c6e17d020d750e3a3c11988c5dfcb
Deyber2000 <garzondeiber6@gmail.com>
2021-02-20 14:07:37 -0500

    skeleton for node server#

---
+++ meadowlark.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const expressHandlebars = require("express-handlebars")
+
+const app = express() 
+const port = process.env.PORT || 3000 
+
+// configure Handlebars view engine
+app.engine('handlebars', expressHandlebars({
+	defaultLayout: 'main',
+	}))
+	app.set('view engine', 'handlebars')
+	
+app.get('/', (req, res) => {
+	res.type('text/plain')
+	res.send('Meadowlark Travel');
+	})
+app.get('/about', (req, res) => {
+	res.type('text/plain')
+	res.send('About Meadowlark Travel')
+	})
+app.use((req, res) => {
+	res.type("text/plain"),
+	res.status(404)
+	res.send("404 - not found")
+})
+
+app.use((err, req, res, next) => {
+	console.error(err.message)
+	res.type("text/plain")
+	res.status(500)
+	res.send("500 - Server error")
+})
+
+app.listen(port, () => console.log(
+	`Express started on http://localhost:${port}; ` +
+	`press Ctrl-C to terminate.`))
